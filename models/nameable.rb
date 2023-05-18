3fe91b25d114d0bd45220b560e61e822027e27c8
SaneMyburg <sane.myburg@gmail.com>
2023-05-18 03:17:28 +0200

    Rearranged all classes to dedicated files

---
+++ models/nameable.rb
@@ -0,0 +1,5 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'Implement the correct_name method.'
+  end
+end
---
+++ models/person.rb
@@ -0,0 +1,34 @@
+require_relative 'nameable'
+require_relative '../decorators/capitalize_decorator'
+require_relative '../decorators/trimmer_decorator'
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    super()
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def of_age?
+    @age >= 18
+  end
+end
+
+person = Person.new(22, name: 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
